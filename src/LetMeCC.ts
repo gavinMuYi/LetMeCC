3bf331f3cce9acd8f61f41d973b634660e26aa47
gavinMuYi <1446623432@qq.com>
2021-07-04 01:32:19 +0800

    track test

--- src/LetMeCC.ts
+++ src/LetMeCC.ts
@@ -2,3 +2,20 @@ import { Greeter } from './test';
    
 let greeter = new Greeter("Hansen");
 alert(greeter.greet());
+
+class LetMeCC {
+    _name: string;
+    _options: object;
+    constructor (name: string) {
+        this._name = name;
+    };
+    init (options: object) {
+        this._options = options;
+    };
+}
+
+declare global {
+    interface Window { LetMeCC: any; }
+}
+
+window.LetMeCC = LetMeCC;
